022e5f85c53ea456f88f2949dc772e0c0d5943db
unknown <foziam139@gmail.com>
2024-03-08 06:52:24 -0800

    added lab 2 classes and objects

---
+++ LAB-2-Classes-and-objects.dart
@@ -0,0 +1,23 @@
+import 'person.dart';
+
+class Student extends Person {
+  int rollNumber;
+  List<int> marks;
+
+  Student(String name, int age, String address, this.rollNumber, this.marks)
+      : super(name, age, address);
+
+  int calculateTotalMarks() {
+    int total = 0;
+    for (int mark in marks) {
+      total += mark;
+    }
+    return total;
+  }
+
+  double calculateAverageMarks() {
+    int total = calculateTotalMarks();
+    double average = total / marks.length;
+    return average;
+  }
+}
