2446ef65fd83d2fe4bfcd839feefd17080feeea5
unknown <foziam139@gmail.com>
2024-03-08 06:28:26 -0800

    added lab 1 variables and data types

---
+++ LAB-1-Variables-and-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "fozia";
+  int age = 21;
+  String favoriteColor = "blue";
+
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $favoriteColor.");
+}
