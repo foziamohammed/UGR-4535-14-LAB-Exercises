b01b3082ee8c668c28499f6e4b2be317a810f739
unknown <foziam139@gmail.com>
2024-03-08 06:46:49 -0800

    added lab 3 control flow

---
+++ LAB-3-Control-flow.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the operator (+, -, *, /):");
+  String operator = stdin.readLineSync()!;
+
+  double result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      print("Invalid operator!");
+      return;
+  }
+
+  print("Result: $result");
+}
