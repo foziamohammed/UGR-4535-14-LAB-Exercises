d0b7de8787ac613b06b5594d2be0782c9bf38cba
unknown <foziam139@gmail.com>
2024-03-08 07:00:23 -0800

    added lab 1 functions and objects

---
+++ LAB-1-Functions.dart
@@ -0,0 +1,11 @@
+int sumNumbers(int num1, int num2) {
+  return num1 + num2;
+}
+
+void main() {
+  int number1 = 5;
+  int number2 = 10;
+
+  int sum = sumNumbers(number1, number2);
+  print("The sum of $number1 and $number2 is: $sum");
+}
